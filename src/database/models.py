3b9fd34456178ed3c029a4f9254fe7d9836f994c
GkiriChen <128749954+GkiriChen@users.noreply.github.com>
2023-11-15 12:52:10 +0200

    update
    add alembic, docker-compose, migration

--- src/database/models.py
+++ src/database/models.py
@@ -1,5 +1,4 @@
 import enum
-import asyncio
 
 from datetime import datetime, date
 
@@ -13,6 +12,13 @@ Base = declarative_base()
 
 
 ## ----Create ----#
+photo_tags = Table(
+    'photo_tags',
+    Base.metadata,
+    Column('photo_id', Integer, ForeignKey('photos.id')),
+    Column('tag_id', Integer, ForeignKey('tags.id'))
+)
+
 
 class UserRole(Base):  #  Не змінювати!
     __tablename__ = "userroles"
@@ -25,9 +31,9 @@ class User(Base):  # Не змінювати!
     id = Column(Integer, primary_key=True)
     role_id = Column('role_id', ForeignKey(
         'userroles.id', ondelete='CASCADE'), default=3)
-    username = Column(String(50))
-    first_name = Column(String(50))
-    last_name = Column(String(50))
+    username = Column(String(50), nullable=False, unique=True)
+    first_name = Column(String(50), nullable=True)
+    last_name = Column(String(50), nullable=True)
     email = Column(String(250), nullable=False, unique=True)
     password = Column(String(255), nullable=False)
     created_at = Column('crated_at', DateTime, default=func.now())
@@ -37,20 +43,11 @@ class User(Base):  # Не змінювати!
     ban = Column(Boolean, default=False)
 
 
-# photo_tag = Table('photo_tag',
-#                  Base.metadata,
-#                  Column("id", Integer, primary_key=True),
-#                  Column("photo", Integer, ForeignKey(
-#                      "photos.id", ondelete="CASCADE")),
-#                  Column("tag", Integer, ForeignKey(
-#                      "tags.id", ondelete="CASCADE")),
-#                  )
-
 class Tag(Base):
     __tablename__ = "tags"
-
     id = Column(Integer, primary_key=True)
-    tag = Column(String(50))
+    tag_name = Column(String(25), unique=True)
+
 
 class Photo(Base):
     __tablename__ = "photos"
@@ -58,12 +55,10 @@ class Photo(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
     user: Mapped["User"] = relationship("User", back_populates="photos")
-    
-    # comments: Mapped[list["Comment"]] = relationship(
-    #     "Comment", back_populates="photo", cascade="all, delete-orphan"
-    # )
-    # tags = relationship("Tag", secondary=photo_tag,
-    #                     backref="photos", passive_deletes=True)
+    tags: Mapped[list["Tag"]] = relationship("Tag", secondary=photo_tags, back_populates="photos")
+    comments: Mapped[list["Comment"]] = relationship(
+        "Comment", back_populates="photo", cascade="all, delete-orphan"
+    )
 
 class Comment(Base):
     __tablename__ = "comments"
@@ -73,11 +68,11 @@ class Comment(Base):
     created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now())
     updated_at: Mapped[date] = mapped_column("updated_at", DateTime, default=func.now(), onupdate=func.now())
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
-    photo_id: Mapped[int] = mapped_column("photos_id", ForeignKey("photos.id", ondelete="CASCADE"), default=None)
+    photo_id: Mapped[int] = mapped_column("photos_id", ForeignKey("photos_id", ondelete="CASCADE"), default=None)
     update_status: Mapped[bool] = mapped_column(Boolean, default=False)
 
-    # user: Mapped[int] = relationship("User", backref="comments")
-    # photo: Mapped["Photo"] = relationship("Photo", back_populates="comments")
+    user: Mapped[int] = relationship("User", backref="comments")
+    photo: Mapped["Photo"] = relationship("Photo", back_populates="comments")
 
 class TransformPhoto(Base):
     __tablename__ = 'transform_photos'
@@ -87,4 +82,3 @@ class TransformPhoto(Base):
     photo_id = Column(Integer, ForeignKey(Photo.id, ondelete="CASCADE"))
 
 
-    
